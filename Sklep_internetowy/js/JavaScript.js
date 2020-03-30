6478a9b9cbd61b3b508bdb327975718641f880d3
kambar98 <kamilbarul123@gmail.com>
2020-03-30 18:17:42 +0200

    Edited clock code

--- Sklep_internetowy/js/JavaScript.js
+++ Sklep_internetowy/js/JavaScript.js
@@ -5,11 +5,11 @@
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-        h + ":" + m + ":" + s;
+    var clock = document.getElementById("clock");
+    clock.innerHTML = h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
-    if (i < 10) { i = "0" + i };  
+    if (i < 10) { i = "0" + i };
     return i;
 }
